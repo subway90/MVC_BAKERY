f2c9bda0a5278740922c3beac29fb12351440583
subway90 <nguyenhieu3105@gmail.com>
2025-01-14 12:57:34 +0700

    add case logout

--- controllers/user/case/dang-xuat.php
+++ controllers/user/case/dang-xuat.php
@@ -1,4 +1,7 @@
 <?php
 
-$_SESSION['user'] = '';
+// huỷ session USER
+unset($_SESSION['user']);
+// quay đến trang đăng nhập
 header('Location:'.URL.'dang-nhap');
+toast_create('success','<i class="bi bi-check-circle me-2"></i> Đăng xuất thành công');
