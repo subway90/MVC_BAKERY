f52f8067128db221f012d7a859f0a06eee1e4feb
subway90 <nguyenhieu3105@gmail.com>
2025-02-05 22:55:21 +0700

    add session cart

--- autoload.php
+++ autoload.php
@@ -5,6 +5,7 @@ session_start();
 
 // Khởi tạo các session
 if(!isset($_SESSION['user'])) $_SESSION['user'] = '';
+if(!isset($_SESSION['cart']) || !is_array($_SESSION['cart'])) $_SESSION['cart'] = [];
 if(!isset($_SESSION['toast']) || !is_array($_SESSION['toast'])) $_SESSION['toast'] = [];
 
 // config
