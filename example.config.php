ed6701d6fdf18e4ecf4f0e1a1b188456aa3dd996
subway90 <nguyenhieu3105@gmail.com>
2025-02-10 22:48:35 +0700

    update example.config

--- example.config.php
+++ example.config.php
@@ -1,13 +1,26 @@
 <?php
 
-const WEB_NAME = 'Bống Béo Bread';
+// const WEB_NAME = 'Bống Béo Bread';
 
-const URL = 'http://localhost:8080/';
+// const URL = 'https://subway90.vn/';
+// const URL_ADMIN = URL.'admin/';
+// const URL_STORAGE = URL."assets/file/";
 
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASS = '';
-const DB_NAME = 'mvc_bongbeobread';
 
-const VNPAY_TMNCODE = '';
-const VNPAY_HASHKEY = '';
+// const DB_HOST = 'localhost';
+// const DB_USER = 'root';
+// const DB_PASS = '';
+// const DB_NAME = 'mvc_bongbeobread';
+
+// const VNPAY_TMNCODE = '';
+// const VNPAY_HASHKEY = '';
+// const VNPAY_URL_RETURN = URL.'/thanh-toan?call-back-vnpay';
+// const VNPAY_URL_SANDBOX = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html';
+
+// const TOAST_TIME = 3000;
+
+// const PATH_FILE_AVATAR = "assets/file/avatar/";
+// const PATH_FILE_MENU = "assets/file/blog/";
+
+// const DEFAULT_IMAGE = 'https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg';
+// const DEFAULT_AVATAR = 'https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png';
