a139c3a4e3960650fcbf9f3d194bde6d71343e0c
subway90 <nguyenhieu3105@gmail.com>
2025-02-12 19:41:43 +0700

    add template admin

--- controllers/admin/thong-ke.php
+++ controllers/admin/thong-ke.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Site</title>
-</head>
-<body>
-<h1>
-    admin dashboard
-</h1>
-</body>
-</html>
+<?php
+
+# [RENDER]
+view('admin','Thống kê','dashboard',null);
