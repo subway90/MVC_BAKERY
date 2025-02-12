6ea9ce3bd253eba76571f6764138ceb0cb9eccc6
subway90 <nguyenhieu3105@gmail.com>
2025-02-12 18:05:46 +0700

    add case test

---
+++ controllers/user/test.php
@@ -0,0 +1,25 @@
+<?php
+
+# [HEADER]
+header('Content-Type: application/json');
+
+# [DATA]
+$data = [
+    "vnpay" => [
+        "number" => "9704198526191432198",
+        "name" => "NGUYEN VAN A",
+        "date" => "07/15",
+        "otp" => 123456,
+        "docs" => "https://sandbox.vnpayment.vn/apis/vnpay-demo/"
+    ],
+    "momo" => [
+        "number" => "9704000000000018",
+        "name" => "NGUYEN VAN A",
+        "date" => "03/07",
+        "otp" => "OTP",
+        "docs" => "https://developers.momo.vn/v3/vi/docs/payment/onboarding/test-instructions/#th%C3%B4ng-tin-test-th%E1%BA%BB-atm"
+    ]
+];
+
+# [RENDER]
+echo json_encode($data);
