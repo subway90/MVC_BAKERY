59eff89c4341c1e9176fb46645c90317cf587598
subway90 <nguyenhieu3105@gmail.com>
2025-02-14 10:35:15 +0700

    update model admin/category : update condition deleted_at IS NULL for select

--- models/admin/category.php
+++ models/admin/category.php
@@ -24,7 +24,7 @@ function get_all_category($condition_deleted) {
 function get_one_category_by_id($id) {
     return pdo_query_one(
         'SELECT * FROM category_product 
-        WHERE status_category_product = 1 
+        WHERE deleted_at IS NULL 
         AND id_category_product = '.$id
     );
 }
@@ -37,8 +37,8 @@ function get_one_category_by_id($id) {
 function check_name_category_product_exist($name) {
     $id = pdo_query_value(
         'SELECT id_category_product FROM category_product 
-        WHERE slug_category_product ="'.create_slug($name).'"
-        AND status_category_product = 1'
+        WHERE deleted_at IS NULL
+        AND slug_category_product ="'.create_slug($name).'"'
     );
     if($id) return 0;
     return 1;
