e793c59900413325274620567402fdb3ba67f47f
subway90 <nguyenhieu3105@gmail.com>
2025-02-14 22:29:00 +0700

    update case admin/quan-li-san-pham : add feature delete & restore

--- controllers/admin/quan-li-san-pham.php
+++ controllers/admin/quan-li-san-pham.php
@@ -10,6 +10,38 @@ $status_page = true; // trạng thái trang (danh sách hoạt động, danh sá
 // Xem danh sách xoá
 if(isset($_arrayURL[1]) && $_arrayURL[1] == 'danh-sach-xoa') $status_page = false;
 
+// Xoá sản phẩm
+if(isset($_POST['delete'])) {
+    // lấy input
+    $id = clear_input($_POST['delete']);
+    // kiểm tra tồn tại
+    if(!check_exist_one('product',$id)) toast_create('danger','Sản phẩm ID = '.$id.' không tồn tại');
+    else {
+        // thực hiện xoá
+        delete_one('product',$id);
+        // thông báo toast
+        toast_create('success','Xoá thành công sản phẩm ID ='.$id);
+        // chuyển route
+        route('admin/quan-li-san-pham');
+    }
+}
+
+// Khôi phục sản phẩm
+if(isset($_POST['restore'])) {
+    // lấy input
+    $id = clear_input($_POST['restore']);
+    if(!check_exist_one_in_trash('product',$id)) toast_create('danger','Sản phẩm ID = '.$id.' không tồn tại trong danh sách xoá');
+    else {
+        //
+        // thực hiện khôi phục
+        restore_one('product',$id);
+        // thông báo toast
+        toast_create('success','Khôi phục thành công sản phẩm ID ='.$id);
+        // chuyển route
+        route('admin/quan-li-san-pham/danh-sach-xoa');
+    }
+}
+
 # [DATA]
 // Lấy danh sách danh mục
 if($status_page) $list_product = get_all_product('IS NULL');
