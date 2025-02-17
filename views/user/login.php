156240224458ab161f5eb9a931de299a0a5209ba
subway90 <nguyenhieu3105@gmail.com>
2025-02-17 17:00:56 +0700

    update css view user/login

--- views/user/login.php
+++ views/user/login.php
@@ -2,15 +2,15 @@
 
     <div class="container my-5">
         <div class="d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
-            <div style="backdrop-filter: blur(10px);" class="col-lg-5 border border-2 shadow border-light rounded-3 py-4 bg-light bg-opacity-10" data-aos="fade-up" data-aos-delay="200">
-                <h2 class="text-center mb-3">Đăng nhập</h2>
+            <div style="backdrop-filter: blur(10px);" class="col-lg-5 border border-2 shadow border-light rounded-3 py-4 bg-light bg-opacity-25" data-aos="fade-up" data-aos-delay="200">
+                <h1 class="text-center mb-3">Đăng nhập</h1>
                 <form action="<?= URL ?>dang-nhap<?=($return_checkout_page) ? '/thanh-toan' : ''?>" method="post" role="form" class="">
-                    <div class="row justify-content-center">
-                        <div class="px-0 col-lg-10 col-md-6 form-floating mb-3">
+                    <div class="row justify-content-center px-4 px-lg-5">
+                        <div class="form-floating p-0 mb-3">
                             <input name="username" value="<?=$username?>" type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                             <label for="floatingInput">Username</label>
                         </div>
-                        <div class="px-0 col-lg-10 col-md-6 form-floating">
+                        <div class="form-floating p-0 mb-3">
                             <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
                             <label for="floatingPassword">Mật khẩu</label>
                         </div>
