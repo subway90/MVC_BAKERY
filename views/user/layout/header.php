72797e3aa262d992fa262468f5614d8d9439059e
subway90 <nguyenhieu3105@gmail.com>
2025-02-19 16:30:55 +0700

    update view user/layout/header : add cdn fontawesome icon

--- views/user/layout/header.php
+++ views/user/layout/header.php
@@ -27,6 +27,8 @@
     <!-- Main CSS File -->
     <link href="<?= URL ?>assets/css/main.css" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
     <!-- =======================================================
   * Template Name: Yummy
   * Template URL: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/
