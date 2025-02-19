6e42735a8e02a6c5313b4837c8cbb58e9b445ab0
subway90 <nguyenhieu3105@gmail.com>
2025-02-19 20:59:46 +0700

    update model stringee : update param func send_otp() & handle phone param

--- controllers/user/test-otp.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-model('user','stringee');
-
-echo stringee_send_otp('84968830591','636899');
--- models/user/stringee.php
+++ models/user/stringee.php
@@ -7,11 +7,15 @@ include 'Stringee/StringeeCurlClient.php';
  * Hàm này dùng để gọi voice gửi otp đến số điện thoại
  * @param string $to Số điện thoại người nhận
  * @param string $otp Mã otp
- * @return int trạng thái của cuộc gọi
+ * @return void trạng thái của cuộc gọi
  */
 function stringee_send_otp($to,$otp) {
+    // lấy key
     $keySid = STRINGEE_SID_KEY;
     $keySecret = STRINGEE_SECRET_KEY;
+    // chỉnh sửa số điện thoại về dạng '84xxxxxxxx'
+    $to = substr($to,1); // xoá kí tự đầu chuỗi
+    $to = '84'.$to; // thểm kí tự 84 vào đầu số
 
     $curlClient = new StringeeCurlClient($keySid, $keySecret);
     $url = 'https://api.stringee.com/v1/call2/callout';
@@ -49,7 +53,7 @@ function stringee_send_otp($to,$otp) {
     // echo '$status: ' . $status . '<br>';
     // echo '$content: ' . $content . '<br>';
 
-    return $status;
+    if($status != '200') die('Chức năng gọi voice gửi OTP bị gián đoạn, vui lòng thử lại sau ! <br><a href="'.URL.'">&rarr; Quay về Trang chủ</a>');
 }
 
 
