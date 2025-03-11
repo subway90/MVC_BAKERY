dbdfd02dc7afb36a30bc77f866319c1c53acf305
subway90 <nguyenhieu3105@gmail.com>
2025-03-11 14:10:43 +0700

    change file & key 'order' to 'invoice'

--- controllers/user/don-hang.php
+++ controllers/user/don-hang.php
@@ -1,7 +1,7 @@
 <?php
 
 # [MODEL]
-model('user','order');
+model('user','invoice');
 
 # [VARIABLE]
 $total = 0;
@@ -9,27 +9,27 @@ $total = 0;
 # [HANDLE]
 // lấy id order từ request
 if(isset($_arrayURL[1]) && $_arrayURL[1]) {
-    $idinvoice = $_arrayURL[1];
+    $id_invoice = $_arrayURL[1];
     // kiếm tra xem đơn hàng có tồn tại hay không
-    if(!check_invoice_exist($idinvoice)) view_404('user');
+    if(!check_invoice_exist($id_invoice)) view_404('user');
 }else view_404('user');
 
 
 # [DATA]
 
 // lấy thông tin hoá đơn
-$arrayinvoice = get_one_invoice_by_id($idinvoice);
+$array_invoice = get_one_invoice_by_id($id_invoice);
 
 // tính tổng tiền
-foreach ($arrayinvoice['order_detail'] as $order) {
-    $total += $order['priceinvoice']*$order['quantityinvoice'];
+foreach ($array_invoice['invoice_detail'] as $invoice) {
+    $total += $invoice['price_invoice']*$invoice['quantity_invoice'];
 }
 
 $data = [
     'total' => $total,
-    'order' => $arrayinvoice['order'],
-    'order_detail' => $arrayinvoice['order_detail'],
+    'invoice' => $array_invoice['invoice'],
+    'invoice_detail' => $array_invoice['invoice_detail'],
 ];
 
 # [RENDER
-view('user','Đơn hàng','order',$data);
+view('user','Đơn hàng','invoice',$data);
