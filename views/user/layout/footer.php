3789a7fa3f06f524ddfa6fb6c464d036d14e9b0e
subway90 <nguyenhieu3105@gmail.com>
2025-05-04 01:03:29 +0700

    update footer

--- views/user/layout/footer.php
+++ views/user/layout/footer.php
@@ -1,15 +1,15 @@
 </main>
 
-  <footer id="footer" class="footer dark-background">
+  <footer id="footer" class="footer dark-background pb-3">
 
     <div class="container">
       <div class="row gy-3">
         <div class="col-lg-3 col-md-6 d-flex">
           <i class="bi bi-geo-alt icon"></i>
           <div class="address">
-            <h4>Address</h4>
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
+            <h4>Địa chỉ</h4>
+            <p>215 Đường Nguyễn Tất Thành, Phường Gia Cẩm</p>
+            <p>Thành phố Việt Trì</p>
             <p></p>
           </div>
 
@@ -18,10 +18,10 @@
         <div class="col-lg-3 col-md-6 d-flex">
           <i class="bi bi-telephone icon"></i>
           <div>
-            <h4>Contact</h4>
+            <h4>Liên hệ</h4>
             <p>
-              <strong>Phone:</strong> <span>+1 5589 55488 55</span><br>
-              <strong>Email:</strong> <span>info@example.com</span><br>
+              <strong>Phone:</strong> <span>0365 286 851</span><br>
+              <strong>Email:</strong> <span>bongbeobread@gmail.com</span><br>
             </p>
           </div>
         </div>
@@ -29,21 +29,17 @@
         <div class="col-lg-3 col-md-6 d-flex">
           <i class="bi bi-clock icon"></i>
           <div>
-            <h4>Opening Hours</h4>
+            <h4>Giờ mở cửa</h4>
             <p>
-              <strong>Mon-Sat:</strong> <span>11AM - 23PM</span><br>
-              <strong>Sunday</strong>: <span>Closed</span>
+              <strong>Thứ 2 - CN:</strong> 6H - 20H
             </p>
           </div>
         </div>
 
         <div class="col-lg-3 col-md-6">
-          <h4>Follow Us</h4>
+          <h4>Theo dõi</h4>
           <div class="social-links d-flex">
-            <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
-            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            <a href="https://fb.com/BongBeo.Bread" target="_blank" class="facebook"><i class="bi bi-facebook"></i></a>
           </div>
         </div>
 
@@ -51,23 +47,12 @@
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Yummy</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> Distributed by <a href="https://themewagon.com">ThemeWagon</a>
-      </div>
+      <p>© <?= date('Y') ?> - <span>Bản quyền thuộc</span><strong class="px-1 sitename">Bống Béo Bread</strong></p>
     </div>
-
   </footer>
 
   <!-- Scroll Top -->
   <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Preloader -->
-  <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
   <script src="<?=URL?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
