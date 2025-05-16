ddcd1c5f8ebc9e7207425fafaf303ebe6e9ca8c6
subway90 <nguyenhieu3105@gmail.com>
2025-05-17 00:35:22 +0700

    update admin : delete feature delete_force

--- controllers/admin/quan-li-san-pham.php
+++ controllers/admin/quan-li-san-pham.php
@@ -42,26 +42,6 @@ if(isset($_POST['restore'])) {
     }
 }
 
-// Xoá vĩnh viễn sản phẩm
-if(isset($_POST['delete_force'])) {
-    // lấy id
-    $id = clear_input($_POST['id_force']);
-    // kiểm tra tồn tại
-    if(!check_exist_one_in_trash('product',$id)) toast_create('danger','Sản phẩm ID = '.$id.' không tồn tại trong danh sách xoá');
-    // kiểm tra có hoá đơn hay không
-    if(pdo_query_value('SELECT COUNT(id_invoice_detail) FROM invoice WHERE id_product = '.$id)) toast_create('danger','Sản phẩm này đã có hoá đơn, không thể xoá !');
-    else {
-        // xoá ảnh của sản phẩm đó nếu có
-        $image = pdo_query_value('SELECT image_product FROM product WHERE id_product = '.$id);
-        if($image) delete_file($image);
-        // thực hiện xoá sản phẩm
-        delete_force_one('product',$id);
-        // thông báo toast và chuyển route
-        toast_create('success','Xoá vĩnh viễn thành công sản phẩm ID = '.$id);
-    }
-    route('admin/quan-li-san-pham/danh-sach-xoa');
-}
-
 # [DATA]
 // Lấy danh sách danh mục
 if($status_page) $list_product = get_all_product('IS NULL');
